434f5db26e5f5fec8ca318bd709211e735ec0c3e
Vikas24Chauhan <vikas24chauhan@gmail.com>
2025-11-27 22:29:04 +0530

    button added in OurBeverage section

--- src/components/ourBeverage/OurBeverage.jsx
+++ src/components/ourBeverage/OurBeverage.jsx
@@ -10,73 +10,6 @@ import Beverage7 from "../../assets/images/home/beverage-7.jpg";
 import Beverage8 from "../../assets/images/home/beverage-8.jpg";
 import Divider from "../../assets/images/home/divider_img.png";
 
-// function OurBeverage() {
-//   const beverageData = [
-//     {
-//       image: Beverage1,
-//       name: "Milkshake",
-//       count: "3 products",
-//     },
-//     {
-//       image: Beverage2,
-//       name: "Frappe",
-//       count: "3 products",
-//     },
-//     {
-//       image: Beverage3,
-//       name: "Boba Drinks",
-//       count: "3 products",
-//     },
-//     {
-//       image: Beverage4,
-//       name: "Mojito",
-//       count: "3 products",
-//     },
-//     {
-//       image: Beverage5,
-//       name: "Slushy",
-//       count: "3 products",
-//     },
-//     {
-//       image: Beverage6,
-//       name: "Hot Chocolate",
-//       count: "3 products",
-//     },
-//     {
-//       image: Beverage7,
-//       name: "smoothie",
-//       count: "3 products",
-//     },
-//     {
-//       image: Beverage8,
-//       name: "coffee",
-//       count: "3 products",
-//     },
-//   ];
-
-//   return (
-//     <div className="beverage-container">
-//       <h5>Best for You</h5>
-//       <h1>
-//         Our <span>Beverage</span>
-//       </h1>
-//       <img src={Divider} alt="" />
-
-//       <div className="beverage-cards">
-//         {beverageData.map((data, index) => (
-//           <div className="beverage-card" key={index}>
-//             <img src={data.image} alt="" />
-//             <h4>{data.name}</h4>
-//             <h6>{data.count}</h6>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default OurBeverage;
-
 function OurBeverage() {
   const beverageData = [
     { image: Beverage1, name: "Milkshake", count: "3 products" },
@@ -108,6 +41,8 @@ function OurBeverage() {
           </div>
         ))}
       </div>
+
+      <button>View All Products</button>
     </div>
   );
 }
